d0334abad3254b300fe1b892c5e861abd17085ce
Michiel Sikkes <michiel.sikkes@gmail.com>
2011-12-21 10:37:04 +0100

    Start with our test suite. Basics set up but does not contain any real tests yet.

---
+++ test/test_builder.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require 'groundcontrol'
+
+module GroundControl
+  
+  class BuilderTest < Test::Unit::TestCase
+    
+    def test_our_test_work
+      assert true
+    end
+  end
+  
+end
